64c3d7445fd2773c7bb749130f0fa7fc9d3adfc7
uverodev <60054614+uverodev@users.noreply.github.com>
2021-05-10 14:56:10 -0400

    commit9

--- app.py
+++ app.py
@@ -23,6 +23,33 @@ def receive_message():
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
+            if message.get['message'].get('text'):
+                #Facebook Messenger ID for user so we know where to send response back to
+                recipient_id = message['sender']['id']
+                if message['message'].get('text'):
+                    response_sent_text = get_message()
+                    send_message(recipient_id, response_sent_text)
+                #if user sends us a GIF, photo,video, or any other non-text item
+                if message['message'].get('attachments'):
+                    response_sent_nontext = get_message()
+                    send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+    
+@app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook.""" 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
+       output = request.get_json()
+       print(output)
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
             if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
